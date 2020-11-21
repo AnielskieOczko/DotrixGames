a03e58b12f33ea76bdb265b8e003cf8f8651ca18
AnielskieOczko <rafaljankowski7@gmail.com>
2020-11-21 18:14:38 +0100

    Initial Commit

---
+++ DotrixGames/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DotrixGames
+//
+//  Created by Rafal on 21/11/2020.
+//  Copyright © 2020 RafalDX. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
