0b99cdc6ef5a141484843e6a994648b7119c4fdb
AnielskieOczko <rafaljankowski7@gmail.com>
2020-11-24 22:23:34 +0100

    Initial commit

--- DotrixGames/ContentView.swift
+++ DotrixGames/ContentView.swift
@@ -7,15 +7,198 @@
 //
 
 import SwiftUI
+import Combine
+import MapKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        TabView {
+            mapDisplayView()
+            //loginView()
+            //createEvent()
+        }
+        
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct mapDisplayView: View {
+    @State private var locations = [MKPointAnnotation]()
+    @State private var newPinCoordinate = CLLocationCoordinate2D()
+    
+    var body: some View {
+        ZStack {
+            mapView(annotations: locations, newPinCoordinate: $newPinCoordinate)
+                .edgesIgnoringSafeArea(.all)
+                    VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    // button to add new annotation on map
+                    // this button will be replaced
+                    // long tap gesture will trigger adding new annotation to map
+                    Button(action: {
+                        let newLocation = MKPointAnnotation()
+                        // to add possibility to custom title, subtitle and others by user
+                        newLocation.title = "Miasto Dotrixa"
+                        newLocation.subtitle = "miasto"
+                        newLocation.coordinate = self.newPinCoordinate
+                        self.locations.append(newLocation)
+                    }) {
+                        Image(systemName: "plus")
+                        }
+                        .padding()
+                        .background(Color.black.opacity(0.75))
+                        .foregroundColor(Color.white)
+                        .font(Font.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
+                }
+            }
+        }
+        .tabItem {
+            Text("Map")
+            Image(systemName:"map.fill")
+        }
     }
 }
+
+struct addLocationButton: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                Button(action: {
+                    
+                }) {
+                    Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(Color.black.opacity(0.75))
+                    .foregroundColor(Color.white)
+                    .font(Font.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+            }
+        }
+    }
+}
+
+// describes event object
+class EventList: ObservableObject {
+    var didChange = PassthroughSubject<Void, Never>()
+    
+    var eventName = "" { didSet{ update() } }
+    static let eventTypes = ["casual","tournament"]
+    var type = 0 { didSet{ update() } }
+    
+    @Published var minPlayerNumber = 0 { didSet{ update() } }
+    @Published var maxPlayerNumber = 0 { didSet { update() } }
+    
+    var eventLocation = "" { didSet{ update() } }
+    var eventDescription = "" { didSet{ update() } }
+    
+    
+    func update() {
+        didChange.send(())
+    }
+}
+
+
+struct createEventView: View {
+    @ObservedObject var event = EventList()
+    
+    var body: some View {
+        NavigationView {
+            
+            Form {
+            // type event name
+            
+            // pick event type
+                Picker(selection: $event.type, label: Text("Chose event type")) {
+                    ForEach(0..<EventList.eventTypes.count) {
+                        Text(EventList.eventTypes[$0]).tag($0)
+                    }
+                }
+            // type min player number
+                Stepper(value: $event.minPlayerNumber, in: 0...20) {
+                    Text("Choose player minimum player number:  \(event.minPlayerNumber)")
+                }
+                
+            // type max player number
+                Stepper(value: $event.maxPlayerNumber, in: 0...20) {
+                    Text("Choose max player number: \(event.maxPlayerNumber)")
+                }
+            
+            // add event location
+            // here will be creared button to open map and give possibility to mark event location
+            // ? read adress based on annotation location, reverse option to add adress and based on adress add annotation
+            
+            // add event description
+            // field so small... to change somehowe field size to be multiline and larger
+                TextField("Add description",text: $event.eventDescription)
+            }
+            .navigationBarTitle("Add new event")
+        }
+    }
+}
+
+struct createEvent: View {
+    var body: some View {
+        createEventView()
+        .tabItem {
+            Text("createView")
+            Image(systemName: "tv.fill")
+        }
+    }
+}
+
+
+
+class LoginData {
+    var userName = ""
+    var password = ""
+}
+
+struct loginButton: View {
+    var body: some View {
+        Text("LOGIN")
+            .font(.headline)
+            .foregroundColor(Color.white)
+            .padding()
+            .frame(width: 100, height: 50)
+            .background(Color.green)
+            .cornerRadius(5)
+        
+    }
+}
+
+struct loginView: View {
+    @State var userName = ""
+    @State var password = ""
+    
+    var body: some View {
+        VStack {
+            TextField("userName", text: $userName)
+            SecureField("password", text: $password)
+            
+            Button(action: { print("login button tapped") }) {
+                loginButton()
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DotrixGames/MapView.swift
@@ -0,0 +1,77 @@
+//
+//  MapView.swift
+//  DotrixGames
+//
+//  Created by Rafal on 23/11/2020.
+//  Copyright © 2020 RafalDX. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct mapView: UIViewRepresentable {
+    
+    // list of annotations
+    var annotations: [MKAnnotation]
+    // store here new pin coordinates taken from long tap location on map
+    @Binding var newPinCoordinate: CLLocationCoordinate2D
+    
+    // class created to control map
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: mapView
+        
+        init(_ parent: mapView) {
+            self.parent = parent
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            print(mapView.centerCoordinate)
+            //parent.centerCoordinator = mapView.centerCoordinate
+        }
+        // func get coordinates from place where long tap gesture detected and store coordinates in newPinCoordinator
+        @objc func addAnnotationLongPress(_ gestureRecognizer: UILongPressGestureRecognizer) {
+            if gestureRecognizer.state == .ended {
+                let selectedPoint = gestureRecognizer.location(in: gestureRecognizer.view)
+                let newCoordinates = (gestureRecognizer.view as? MKMapView)?.convert(selectedPoint, toCoordinateFrom: gestureRecognizer.view)
+                let newAnnotation = MKPointAnnotation()
+                newAnnotation.coordinate = newCoordinates!
+                //parent.annotations.append(newAnnotation)
+                parent.newPinCoordinate = newCoordinates!
+                print("longPress: \(newCoordinates!)")
+            }
+        }
+        // when this function uncommented adding annotations does not works
+        /*
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let view = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            view.canShowCallout = true
+            return view
+        }
+        */
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        
+        // define long press gesture functionality and add it to the view
+        let longPress = UILongPressGestureRecognizer(target: context.coordinator, action: #selector(context.coordinator.addAnnotationLongPress(_:)))
+        mapView.addGestureRecognizer(longPress)
+        
+        mapView.delegate = context.coordinator
+        return mapView
+    }
+    
+    func updateUIView(_ view: MKMapView, context: Context) {
+        // checks if annotation count in view is same line in model if not, remove all annotations and replace them by new annotations from model
+        // this should be triggered everytime something changes in view
+        if annotations.count != view.annotations.count {
+            print(annotations.count)
+            view.removeAnnotations(view.annotations)
+            view.addAnnotations(annotations)
+        }
+    }
+}
